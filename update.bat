490d1f6a808b580074914da054e37ce74a6e62e3
Muhammad Hasan <muhammedhasan483@gmail.com>
2023-01-18 01:44:25 +0000

    Add files via upload

---
+++ update.bat
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name project2 --template-body file://project2.yml  --parameters file://parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
